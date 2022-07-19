4a3d2d4afa40a323097f0d60479089d7ae8955ad
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2022-07-19 15:18:45 -0300

    finish of the app

--- upload-api/index.js
+++ upload-api/index.js
@@ -2,18 +2,22 @@ const express = require("express");
 const multer = require("multer");
 
 const app = express();
+
+
 const upload = multer({
-    dest: './uploads/'
+    dest: './uploads/',
 })
 
 
-
-app.post("/upload", upload.single('file'), (req, res) => {
+app.post("/upload", upload.single('file'), ( req, res) => {
     res.json({ file: req.file });
 })
 
-
-
+app.get('/download/:id', function(req, res){
+    const file = `${__dirname}/uploads/${req.params.id}`;
+    res.download(file); // Set disposition and send it.
+  });
+  
 app.listen(3344, () => {
     console.log("App is running on localhost:3344");
 })
